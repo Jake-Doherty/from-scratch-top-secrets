4de4a08f45f2d160f7a8679c1d3a6fbe00efdfcb
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-17 09:12:54 -0800

    setup db for users table and add 1st test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,30 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const testUser = {
+  firstName: 'Test',
+  lastName: 'TestUser',
+  email: 'test@test.com',
+  password: 'asdfasdf',
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should create a new user', async () => {
+    const newUser = await request(app).post('/app/v1/users').send(testUser);
+
+    const { firstName, lastName, email } = testUser;
+
+    expect(newUser.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table
+ if exists users;
+
+create table
+	users (
+		id bigint generated always as identity primary key,
+		first_name varchar not null,
+		last_name varchar not null,
+		email varchar not null,
+		password_hash varchar not null
+	);
